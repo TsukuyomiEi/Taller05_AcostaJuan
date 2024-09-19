f04fff83894df1c77082a2561d0d255ea47a9908
unknown <rangelmaciel21@gmail.com>
2024-09-19 11:24:28 -0500

    m1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import miLogo from "../src/assets/img/imagen_principal.png"
+import './App.css'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <img src={miLogo} alt='Logo de React' />
+        <h1> El Cerezo Sagrado </h1>
+        <p>Hogar del Trueno</p>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
